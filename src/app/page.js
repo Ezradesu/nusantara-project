a2a9e386aa6e48dd94cd22ffd1296490433cc1cd
Ezradesu <ezraejer@gmail.com>
2024-02-20 14:35:05 +0700

    lagi ngerjain homenya sabar ya bang

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,28 @@
+"use client";
 import Image from "next/image";
 
 export default function Home() {
   return (
-  <h1 className="uppercase">ini adalah title</h1>
+    <div className="page-parent">
+      <div className="content w-full h-screen relative px-0 py-4">
+      <Image
+      src="https://www.humaniora.id/wp-content/uploads/2023/10/tari-bali.jpg"
+      width={1000}
+      height={1000}
+      className="w-4/6 rounded-lg items-center m-auto"
+      />
+      </div>
+
+      <div className="hero overflow-hidden">
+        <h1 className="text-9xl font-medium m-10 items-center italic px-7 py-7">
+          sebuah website yang mengajak kamu untuk mengenal keragaman budaya Indonesia
+        </h1>
+      </div>
+      <div className="py-60"></div>
+
+      <div className="border rounded-full">
+        <h1 className="text-9xl font-medium text-right">KULINER</h1>
+      </div>
+    </div>
   );
 }
--- src/components/SmoothScroll.js
+++ src/components/SmoothScroll.js
@@ -3,6 +3,21 @@
 import { ReactLenis } from '@studio-freight/react-lenis'
 
 function SmoothScroll({children}) {
+  // function Component() {
+  //   const lenisRef = useRef()
+    
+  //   useEffect(() => {
+  //     function update(time) {
+  //       lenisRef.current?.lenis?.raf(time * 1000)
+  //     }
+    
+  //     gsap.ticker.add(update)
+    
+  //     return () => {
+  //       gsap.ticker.remove(update)
+  //     }
+  //   })
+  // }
  
   return (
     <ReactLenis root>
